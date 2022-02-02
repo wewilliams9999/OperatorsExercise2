120786bc85a71d6b04d9cab3f4a7654dc0c10d1a
wewilliams9999 <williamswallise@gmail.com>
2022-02-02 15:16:42 -0600

    initial commit

---
+++ OperatorsExercise2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace OperatorsExercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the radius of your circle?");
+
+            var radius = double.Parse(Console.ReadLine());
+
+            var result = AreaOfCircle(radius);
+
+            Console.WriteLine($"The area of a circle with radius of {radius} is {result}");
+        }
+
+        public static double AreaOfCircle(double radius)
+        {
+            var area = Math.PI * radius *radius;
+            return area;
+        
+        }
+    }
+
+}
